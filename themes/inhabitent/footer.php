fc3dd224ab660d8301141717715d2bae4923109d
Wes <wes.delange@gmail.com>
2018-05-24 14:53:09 -0400

    Created footer.scss and restyled footer, added variables

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -6,34 +6,36 @@
  */
 
 ?>
+		</div><!-- #content -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class='container'>
+		<div class="contact-info">
+			<h3>contact info</h3>
+				<p><i class="fa fa-envelope"></i>info@inhabitent.com</p>
+				<p><i class="fa fa-phone"></i>778-888-7777</p>
+				<p><i class="fab fa-facebook-square"></i>
+					<i class="fab fa-twitter-square"></i>
+					<i class="fab fa-google-plus-square"></i>
+				</p>
+		</div>
 
-			</div><!-- #content -->
+		<div class="business-hours">
+			<h3>business hours</h3>
+				<p><span>Monday to Friday:</span> 9am to 5pm</p>
+				<p><span>Saturday:</span> 10am to 2pm</p>
+				<p><span>Sunday:</span> Closed</p>
+		</div>
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="footer-container">
-
-				<div class="contact-info">
-					<h3>contact info</h3>
-					<p class="email">info@inhabitent.com</p>
-					<p class="phone">778-888-7777</p>
-					<p><span class="facebook"></span><span class="twitter"></span><span class="google"></span></p>
-				</div>
-
-				<div class="business-hours">
-					<h3>business hours</h3>
-					<p>Monday to Friday: 9am to 5pm</p>
-					<p>Saturday: 10am to 2pm</p>
-					<p>Sunday: Closed</p>
-				</div>
-
-				<div class="footer-logo">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" 
-					alt="logo" width="280px" height="auto" /> 	
-				</div>
-
-				</div>
-				<div class="site-info">
-					<p>Copywright</p>
+		<div class="footer-logo">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-text.svg" alt="logo"/> 	
+		</div>
+		<div class="site-info">
+			<p>Copywright &copy;2018 Inhabitent</p>
+		</div>					
+	</div>
+	
+	<!--end of footer-->
+		
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -97,5 +97,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
