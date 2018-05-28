200e763baa9657aca189964be7ed694a054ff269
Wes <wes.delange@gmail.com>
2018-05-28 13:10:52 -0400

    styled single-product page

--- themes/inhabitent/single-products.php
+++ themes/inhabitent/single-products.php
@@ -12,32 +12,46 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
+			
+			
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+			
+			<div class="single-product-container">
+				
+			<div class="img-container">
+				<header class="entry-header">
 
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+						<?php endif; ?>
+					
+				</header><!-- .entry-header -->
+			</div>
+				
+			<div class="description-container">
+					
+					<div class="entry-content">
+						
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						
+						<span><?php echo CFS()->get( 'price' ); ?></span><!--custom field suite -->
+
+						<?php the_content(); ?>
+					
+					</div><!-- .entry-content -->
+			
+
+					<footer class="entry-footer">
+						<button><i class="fab fa-facebook-f"></i> like</button>
+						<button><i class="fab fa-twitter"></i> tweet</button>
+						<button><i class="fab fa-pinterest"></i> pin</button>
+					</footer><!-- .entry-footer -->
+				
+			</div><!-- description container -->
 		
-
-
-	</header><!-- .entry-header -->
-
-	
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-			<!--custom field suite -->
-			<p><?php echo CFS()->get( 'price' ); ?></p>
-		</div><!-- .entry-content -->
-	
-
-	<footer class="entry-footer">
-<!--social media links-->
-	</footer><!-- .entry-footer -->
-
-</article><!-- #post-## -->
-
+		</div><!-- single-container -->
+		
+		</article><!-- #post-## -->
 
 		<?php endwhile; // End of the loop. ?>
 
