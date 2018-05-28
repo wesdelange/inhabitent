f05b218c09113e7d838dd659f0cc07454cd0ab5f
Wes <wes.delange@gmail.com>
2018-05-28 10:19:27 -0400

    styled header, fixed variables issue

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -7,14 +7,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="front-page-content-area">
 		<main id="main" class="site-main" role="main">
 
-	<section class="hero">
-		<div class="hero-image">
-			<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg">
-		</div>
-	</section>
+			<div class="hero">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg">
+			</div>
 		
 		
 		
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,24 +21,27 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php ( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<div class="logo">
-						<a href="<?php  echo esc_url( home_url( '/' ) ); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent.svg">
-						</a>
-					</div>
-						
-					<div class="menu-list">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					</div>	
 				
-				</nav><!-- #site-navigation -->
+				<div class="menu-container">
+					
+					<div class="site-branding">
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<p class="site-description"><?php ( 'description' ); ?></p>
+					</div><!-- .site-branding -->
+						
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+							
+					<a href="<?php  echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/logos/inhabitent-logo-tent.svg">
+					</a>
+							
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+								
+						
+					</nav><!-- #site-navigation -->
+
+				</div><!-- menu-container -->
 			
 			</header><!-- #masthead -->
 
